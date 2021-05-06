df88ad592b402a66437ae03afd5973a35bc94595
duyvd <duy.vd176936@gmail.com>
2021-05-06 11:05:08 +0700

    test3

--- test.cpp
+++ test.cpp
@@ -3,11 +3,7 @@ main()
 {
     printf("hello...");
 
-<<<<<<< HEAD
-    int a=0;a++;
-=======
     int a=0; scanf("%d",&a);
->>>>>>> c75d0d8a3f6b6479c1ee5431bcf8d7a9e2c12cfa
     
     
     fflush(stdin);
